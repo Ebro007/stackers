28b65f6843a87352a48ea17874fe00d9aa70fd91
Ebro007 <ebrokramer@yahoo.com>
2018-07-25 14:05:07 -0500

    pixel stops with user input

--- stackers.py
+++ stackers.py
@@ -14,21 +14,27 @@ class stack():
 
 	def startGame(self):
 		x =0
-		pygame.time.set_timer(USEREVENT +1, 200)
+		pygame.time.set_timer(USEREVENT +1, 800)
 		while self.gaming:
 			for event in pygame.event.get():
-				sense.set_pixel(x, 7, (255, 0, 125))
-				time.sleep(.2)
-				sense.set_pixel(x, 7, (0, 0, 0))
-				if x == 7:
-					x = 0
+				if event.type == KEYDOWN:
+					sense.set_pixel(x, 7, (255, 0, 125))
+					self.gaming = False
+					print 'hi'	
 				else:
-					x = x +1
+					sense.set_pixel(x, 7, (255, 0, 125))
+					time.sleep(0.3)
+					sense.set_pixel(x, 7, (0, 0, 0))
+					time.sleep(0.3)
+					if x == 7:
+						x = 0
+					else:
+						x = x +1
+				
 				
 				
 if __name__=="__main__":
-	try:
-		
+	try:	
 		game = stack()
 		game.startGame()
 	except KeyboardInterrupt:
