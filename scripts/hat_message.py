e8d0049b7a09cbadaff1cb476419c69314889929
Ebro007 <ebrokramer@yahoo.com>
2018-07-25 11:09:40 -0500

    Scripts used for the stackers.py game

---
+++ scripts/hat_message.py
@@ -0,0 +1,16 @@
+import time
+from sense_hat import SenseHat
+sense = SenseHat()
+
+yellow = (255, 255, 0)
+blue = (0, 0, 225)
+
+speed = 0.05
+message = 'Hello World'
+
+sense.show_message(message, speed, text_colour=yellow, back_colour=blue)
+time.sleep(1)
+sense.show_letter('!', yellow)
+time.sleep(1)
+sense.clear()
+
