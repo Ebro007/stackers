e1eb4ee55f71934f2c706838e2669df266d58ad8
Ebro007 <ebrokramer@yahoo.com>
2018-07-25 15:19:43 -0500

    Finished game

--- stackers.py
+++ stackers.py
@@ -15,17 +15,37 @@ class stack():
 	def startGame(self):
 		x =0
 		y =7
+		win =0
 		pygame.time.set_timer(USEREVENT +1, 800)
 		while self.gaming:
 			for event in pygame.event.get():
 				if event.type == KEYDOWN:
+					if y ==7:
+						win = x
+					else:
+						if y == 0:
+							if x == win:
+								print 'You Win!'
+								print 'Congratulations!'
+								self.gaming = False
+								sense.clear()
+						else:
+							if x == win:
+								print 'Great!'
+							else:
+								print 'You Lose!'
+								self.gaming = False
+								sense.clear()
+
 					sense.set_pixel(x, y, (255, 0, 125))
-					self.gaming = False
-					print 'hi'
-					y = y-1
-					if self.gaming == False:
-						self.gaming = True
+#					self.gaming = False
+#					print 'hi'
+					if self.gaming == True:					
+						y = y-1
 						x = 0
+#					if self.gaming == False:
+#						self.gaming = True
+						
 					
 				else:
 					sense.set_pixel(x, y, (255, 0, 125))
@@ -36,8 +56,6 @@ class stack():
 						x = 0
 					else:
 						x = x +1
-#		if self.gaming == False
-#			self.gaming = True
 	
 				
 				
